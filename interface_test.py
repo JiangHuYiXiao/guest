6161dd42d43605add00507652193d93a48a98425
JiangHuYiXiao <1163270704@qq.com>
2021-05-22 10:27:21 +0800

    interface_test

---
+++ interface_test.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2021/5/21 17:07
+# @Software       : guest
+# @Python_verison : 3.7
+import requests
+
+# 发布会查询接口
+url = 'http://127.0.0.1:8000/api/search_event/'
+r = requests.get(url,{'eid':1})
+result = r.json()
+print(result)
+# 断言接口返回值
+assert result['status'] == 200
+assert result['message'] == 'query success'
+assert result['data']['name'] == '小米发布会1'
+assert result['data']['address'] =='北京'
