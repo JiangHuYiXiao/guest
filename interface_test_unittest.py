8cec229b045ab9df84476fa9035b723524ab18f8
JiangHuYiXiao <1163270704@qq.com>
2021-05-25 08:59:12 +0800

    接口自动测试框架

--- interface_test_unittest.py
+++ interface_test_unittest.py
@@ -22,5 +22,23 @@ class Test_search_event(unittest.TestCase):
         self.assertEqual(result['status'],10021)
         self.assertEqual(result['message'],'parameter error')
 
+    def test_get_event_empty(self):
+        '''eid不存在'''
+        r = requests.get(self.url,params={'eid':2000})
+        result = r.json()
+        self.assertEqual(result['status'],10022)
+        self.assertEqual(result['message'],'query result is empty')
+
+    def test_get_event_success(self):
+        '''查询成功'''
+        r = requests.get(self.url,params={'eid':1})
+        result = r.json()
+        self.assertEqual(result['status'],200)
+        self.assertEqual(result['message'],'query success')
+        self.assertEqual(result['data']['name'],'小米发布会1')
+        self.assertEqual(result['data']['address'],'北京')
+        self.assertEqual(result['data']['start_time'],'2021-04-09T08:00:00')
+
+
 if __name__ == '__main__':
     unittest.main()
